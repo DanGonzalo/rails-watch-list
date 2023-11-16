a8fea9294f53b31c0b815c48f7fda9db7fab4767
Daniel Gonzalo <danicogonzalo@gmail.com>
2023-11-16 17:19:38 +0800

    processing

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,10 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
   validates :comment, length: { minimum: 6 }
-  validates :movie, uniqueness: { scope: :list,
-    message: "There should only be one movie for one list!" }
+  validates :movie, uniqueness: {
+    scope: :list,
+    message: 'There should only be one movie for one list!'
+  }
 end
